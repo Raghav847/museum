d053c607f15daf73f6f48b367c904928c4e07a7d
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-03-17 01:39:06 -0700

    serve static

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fem/museum
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hELLO gOPHER"))
+}
+
+func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/hello", handleHello)
+
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
+
+	err := http.ListenAndServe(":3333", server)
+	if err != nil {
+		fmt.Println("Err while opening the server")
+	}
+}
